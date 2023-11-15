aae48627540727f7cdc71ed9682ad857ab83fc42
zackcis <zakaria.eldahar@gmail.com>
2023-11-14 18:50:11 -0800

    the params.c file updated

--- params.c
+++ params.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * init_params - this initializes and clears the parameters struct
- * @params: the parameters struct to be initialized
- * @ap: the argument pointer (unused in this function)
- * this function initializes the fields of
- * the parameters struct to default values.
- * the flags are set to 0, width to 0, precision
- * to UINT_MAX, and modifiers to 0.
+ * init_params - this clears struct fields and reset buf
+ * @params: the parameters struct
+ * @ap: the argument pointer
  * Return: void
  */
 
 void init_params(params_t *params, va_list ap)
+
 {
-	/* clear all flags and set default values */
+
 	params->unsign = 0;
+
 	params->plus_flag = 0;
 	params->space_flag = 0;
 	params->hashtag_flag = 0;
@@ -26,7 +24,6 @@ void init_params(params_t *params, va_list ap)
 
 	params->h_modifier = 0;
 	params->l_modifier = 0;
-
-	/* unused argument */
 	(void)ap;
+
 }
